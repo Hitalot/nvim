6f4d4e5ca8ef100e47e80a4845da4069178f4bdb
EviLDgL <>
2024-02-12 23:25:58 +0100

    added mason tool manager

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,22 +1,36 @@
+local python_path = ""
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-	end,
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap"
+        },
+        config = function (_, opts)
+            local path = python_path
+            require("dap-python").setup(path)
+        end
+    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+        end,
+    },
 }
--- lua/plugins/lsp-base.lua
+++ lua/plugins/lsp-base.lua
@@ -1,24 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-    end
-  }
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup({
+            })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright" , "ruff_lsp", "clangd" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                filetypes = {"python"}
+            })
+            lspconfig.ruff_lsp.setup({
+                on_attach = on_attach,
+
+            })
+            lspconfig.clangd.setup({})
+        end
+    },
+    {
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+        requires = {
+            'williamboman/mason.nvim',
+        },
+        config = function()
+            require('mason-tool-installer').setup({
+                ensure_installed = {
+                    'black',
+                    'isort',
+                    'stylua',
+                    'debugpy',
+                },
+            })
+        end,
+    }
 }
