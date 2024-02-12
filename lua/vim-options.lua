e1c0ff1cb20cc7b17d5a72cb578bd4d81b6b7265
EviLDgL <>
2024-02-12 20:30:10 +0100

    added keybinds

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,13 @@
 vim.g.mapleader = " "
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
